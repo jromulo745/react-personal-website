cb347ecfd6975cdf0eb4ef1efac39d16ecd3398f
jromulo745 <85465170+jromulo745@users.noreply.github.com>
2023-03-05 18:11:53 -0800

    implement functionality of IT test

--- src/components/WebApps.js
+++ src/components/WebApps.js
@@ -41,14 +41,7 @@ function WebApps() {
   const [current_choice_3, setChoice3] = useState('');
   const [current_choice_4, setChoice4] = useState('');
   const [current_choice_5, setChoice5] = useState('');
-  
-  // let choice_array = [
-  //   setChoice1,
-  //   setChoice2,
-  //   setChoice3,
-  //   setChoice4,
-  //   setChoice5,
-  // ];
+  const [correct_answer, setAnswer] = useState('');
 
   // load questions text file data
   fetch('/multiple-choice-explanation-template-quiz/questions.txt')
@@ -70,11 +63,11 @@ function WebApps() {
     return showQuestion(true) 
     + shownNextButton(true)
     + setQuestion(questions[counter])
-    + (temp[0][1] === '*' ? setChoice1(temp[0].substring(2)) : setChoice1(temp[0]))
-    + (temp[1][1] === '*' ? setChoice2(temp[1].substring(2)) : setChoice2(temp[1]))
-    + (temp[2][1] === '*' ? setChoice3(temp[2].substring(2)) : setChoice3(temp[2]))
-    + (temp[3][1] === '*' ? setChoice4(temp[3].substring(2)) : setChoice4(temp[3]))
-    + (temp[4][1] === '*' ? setChoice5(temp[4].substring(2)) : setChoice5(temp[4]))
+    + (temp[0][1] === '*' ? (setChoice1(temp[0].substring(2)), setAnswer(temp[0].substring(2))) : setChoice1(temp[0]))
+    + (temp[1][1] === '*' ? (setChoice2(temp[1].substring(2)), setAnswer(temp[1].substring(2))) : setChoice2(temp[1]))
+    + (temp[2][1] === '*' ? (setChoice3(temp[2].substring(2)), setAnswer(temp[2].substring(2))) : setChoice3(temp[2]))
+    + (temp[3][1] === '*' ? (setChoice4(temp[3].substring(2)), setAnswer(temp[3].substring(2))) : setChoice4(temp[3]))
+    + (temp[4][1] === '*' ? (setChoice5(temp[4].substring(2)), setAnswer(temp[4].substring(2))) : setChoice5(temp[4]))
     + setCounter(counter + 1);
   }
   
@@ -82,47 +75,24 @@ function WebApps() {
     let temp = choices[counter].split(',');
     return setQuestion(questions[counter]) 
     + setCounter(counter + 1) 
-    + (temp[0][1] === '*' ? setChoice1(temp[0].substring(2)) : setChoice1(temp[0]))
-    + (temp[1][1] === '*' ? setChoice2(temp[1].substring(2)) : setChoice2(temp[1]))
-    + (temp[2][1] === '*' ? setChoice3(temp[2].substring(2)) : setChoice3(temp[2]))
-    + (temp[3][1] === '*' ? setChoice4(temp[3].substring(2)) : setChoice4(temp[3]))
-    + (temp[4][1] === '*' ? setChoice5(temp[4].substring(2)) : setChoice5(temp[4]))
+    + (temp[0][1] === '*' ? (setChoice1(temp[0].substring(2)), setAnswer(temp[0].substring(2))) : setChoice1(temp[0]))
+    + (temp[1][1] === '*' ? (setChoice2(temp[1].substring(2)), setAnswer(temp[1].substring(2))) : setChoice2(temp[1]))
+    + (temp[2][1] === '*' ? (setChoice3(temp[2].substring(2)), setAnswer(temp[2].substring(2))) : setChoice3(temp[2]))
+    + (temp[3][1] === '*' ? (setChoice4(temp[3].substring(2)), setAnswer(temp[3].substring(2))) : setChoice4(temp[3]))
+    + (temp[4][1] === '*' ? (setChoice5(temp[4].substring(2)), setAnswer(temp[4].substring(2))) : setChoice5(temp[4]))
     + console.log(counter);
   }
+
+  function checkAnswer(choice) {
+    console.log(correct_answer);
+    console.log(choice);
+    if (correct_answer == choice) {
+      alert('correct');
+    }
+  }
+
   //checkpoint
   return (
-    // <div>
-    //   <h2 style={{marginBottom: '10px'}}>File Character Counter</h2>
-    //   <div className={`${styles.box}`} style={{marginLeft: '300px', marginRight: '300px'}}>
-    //       <input style={hover_style} className="box" type="file" onChange={handleFileInputChange} />
-    //   </div>
-    //   <h2 style={{marginBottom: '10px'}}>IT Tester</h2>
-    //   <div className={`${styles.box}`} style={{marginLeft: '300px', marginRight: '300px'}}>
-    //     <div className={`${styles.button} ${styles.button}`}>
-    //       {/* begin test button; null if next button true and content if false */}
-    //       {next_button ? null : (<button style={{marginBottom: '10px', marginTop: '5px'}} onClick={handleClick}>Begin Test</button>)}
-    //       {/* next button */}
-    //       {next_button ? (<button style={{marginBottom: '-10px', marginTop: '5px'}} onClick ={nextQuestion}>Next Question</button>) : null}
-    //     </div>
-    //     {question_cluster ? (<h2 style={{textAlign: 'left', marginLeft: '10px'}}><br />{current_question}</h2>) : null}
-    //     {question_cluster ? ( <div style={{textAlign: 'left', marginLeft: '10px'}} className={`${styles.button} ${styles.button}`}>
-    //       <br />
-    //       <button>{current_choice_1}</button>
-    //       <br />
-    //       <br />
-    //       <button>{current_choice_2}</button>
-    //       <br />
-    //       <br />
-    //       <button>{current_choice_3}</button>
-    //       <br />
-    //       <br />
-    //       <button>{current_choice_4}</button>
-    //       <br />
-    //       <br />
-    //       <button style={{marginBottom: '15px'}}>{current_choice_5}</button> </div>
-    //     ) : null}
-    //   </div>
-    // </div>
     <div className="banner_box banner">
       <h2 style={{marginBottom: '5px'}}>File Character Counter</h2>
       <div className="container">
@@ -143,19 +113,19 @@ function WebApps() {
           {question_cluster ? (<h2 style={{textAlign: 'left', marginLeft: '10px'}}><br />{current_question}</h2>) : null}
           {question_cluster ? ( <div style={{textAlign: 'left', marginLeft: '10px'}} className={`${styles.button} ${styles.button}`}>
             <br />
-            <button>{current_choice_1}</button>
+            <button onClick={() => checkAnswer(current_choice_1)}>{current_choice_1}</button>
             <br />
             <br />
-            <button>{current_choice_2}</button>
+            <button onClick={() => checkAnswer(current_choice_2)}>{current_choice_2}</button>
             <br />
             <br />
-            <button>{current_choice_3}</button>
+            <button onClick={() => checkAnswer(current_choice_3)}>{current_choice_3}</button>
             <br />
             <br />
-            <button>{current_choice_4}</button>
+            <button onClick={() => checkAnswer(current_choice_4)}>{current_choice_4}</button>
             <br />
             <br />
-            <button style={{marginBottom: '15px'}}>{current_choice_5}</button> </div>
+            <button onClick={() => checkAnswer(current_choice_5)} style={{marginBottom: '15px'}}>{current_choice_5}</button> </div>
           ) : null}
         </div>
         {/* ---- */}
