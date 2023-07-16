bd855d32a4d79f9acf224db5b1c8f3193da8ade7
jromulo745 <85465170+jromulo745@users.noreply.github.com>
2023-07-15 19:14:03 -0700

    update info on personal interests

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ function Banner() {
         <div className="container">
           <div className="item">
             <h1 style={{textAlign: 'center'}}>Welcome!</h1>
-            <p>Greetings! My name is Joshua Jefson Romulo, and this is my personal website, programmed from the ground up using ReactJS. I'm a computer science graduate & IT assistant @ CSUEB, and some of my passions in life include computer programming, playing musical instruments, and film appreciation.</p>
+            <p>Greetings! My name is Joshua Jefson Romulo, and this is my personal website, programmed from the ground up using ReactJS. I'm a computer science graduate & IT assistant @ CSUEB, and some of my passions in life include computer programming, playing musical instruments, and video games.</p>
           </div>
         </div>
       </div>
